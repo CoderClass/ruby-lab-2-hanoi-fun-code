ceb5e6fb4e937d75f774aebce1372f7153c3e039
Vu Long <vulong264@gmail.com>
2016-10-18 19:54:35 +0700

    finish milestone 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	helper_method :current_user #provide access to :current_user function for views
+
+
+
+	protected
+	#this function getting data from session object to pass to view layer
+	def current_user
+		return @current_user if @current_user
+		if session[:user_id]
+			@current_user = User.find session[:user_id]
+		end
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+	def create
+		@user = User.new user_params
+		if @user.save
+			flash[:success] = "User created successfully"
+			redirect_to new_session_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :name, :password, :password_confirmation)
+	end
 end
